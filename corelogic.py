7f4a61be958257cd63ace7a70a8977afd79e837e
Santiagoherrella <santiagoherrera@magnetron.com.co>
2025-07-21 19:37:33 -0500

    Update corelogic.py

--- corelogic.py
+++ corelogic.py
@@ -2,11 +2,11 @@ import os
 from langchain_openai import ChatOpenAI
 MODEL_NAME_LLM = "gpt-4.1-nano-2025-04-14"
 
-def get_llm():
+def get_llm(key):
     """Carga y devuelve el modelo LLM de OpenAI."""
     try:
         llm = ChatOpenAI(model_name=MODEL_NAME_LLM, temperature=0.1,
-                         api_key=st.secrets["OPENAI_API_KEY"],
+                         api_key=key,
                          max_tokens=1500)
         return llm
     except Exception as e:
