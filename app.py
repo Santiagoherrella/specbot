3cbb5fb181bd22924d6de4ad859c9c1c476cc9b8
Santiagoherrella <santiagoherrera@magnetron.com.co>
2025-07-21 19:38:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -35,9 +35,9 @@ EXECUTIVE_SUMMARY_PROMPT = get_prompt_summary_str ()
 CUSTOM_RAG_DOC_PROMPT = get_prompt_RAG_str()
 
 # --- Funciones Cacheadas ---
-
+key = st.secrets["OPENAI_API_KEY"]
 @st.cache_resource
-def cached_get_llm():
+def cached_get_llm()key:
     return get_llm()
 
 
