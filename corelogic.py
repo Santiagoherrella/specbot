23c88fad1fbb383389d66ddbfd830089135a3401
Santiagoherrella <santiagoherrera@magnetron.com.co>
2025-07-24 18:06:53 -0500

    Update corelogic.py

--- corelogic.py
+++ corelogic.py
@@ -1,13 +1,12 @@
 import os
-from langchain_openai import ChatOpenAI
-MODEL_NAME_LLM = "gpt-4.1-nano-2025-04-14"
+from langchain_google_genai import ChatGoogleGenerativeAI
+MODEL_NAME_LLM = "models/gemini-2.0-flash"
 
 def get_llm(key):
     """Carga y devuelve el modelo LLM de OpenAI."""
-    try:
-        llm = ChatOpenAI(model_name=MODEL_NAME_LLM, temperature=0.1,
-                         api_key=key,
-                         max_tokens=1500)
+    try:llm = ChatGoogleGenerativeAI(model=MODEL_NAME_LLM, 
+                                         temperature=0.3,
+                                         google_api_key=key)
         return llm
     except Exception as e:
         print(f"Error al inicializar LLM de OpenAI: {e}")
