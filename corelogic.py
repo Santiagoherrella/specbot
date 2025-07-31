b31c00da632f177dd179b251306bfaae941cfaba
Santiagoherrella <santiagoherrera@magnetron.com.co>
2025-07-31 09:31:56 -0500

    Update corelogic.py

--- corelogic.py
+++ corelogic.py
@@ -7,6 +7,7 @@ import json
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_openai import ChatOpenAI
+import streamlit as st
 # Cargar variables de entorno (.env)
 load_dotenv()
 
@@ -17,37 +18,22 @@ with open("config.json", "r", encoding="utf-8") as f:
 LLM_PROVIDER = config.get("llm_provider").lower()
 MODEL_NAME_LLM = config.get("llm_model")
 TEMPERATURE = config.get("temperature")
-
 def get_llm():
-    
+    import streamlit as st
     if LLM_PROVIDER == "openai":
-        """Carga y devuelve el modelo LLM de OpenAI."""
-        if 'OPENAI_API_KEY' not in os.environ:
-            raise ValueError("La variable de entorno OPENAI_API_KEY no está configurada.")
-        print(f"Inicializando LLM de OpenAI: {MODEL_NAME_LLM}...")
-        try:
-            llm = ChatOpenAI(
-                            model=MODEL_NAME_LLM,
-                            temperature=TEMPERATURE, 
-                            api_key=os.getenv("OPENAI_API_KEY")
-                            )
-            return llm
-        except Exception as e:
-            print(f"Error al inicializar LLM de OpenAI: {e}")
-            raise
+       api_key = st.secrets["OPENAI_API_KEY"]
+        return ChatOpenAI(
+            model=MODEL_NAME_LLM,
+            temperature=TEMPERATURE,
+            api_key=api_key
+            )        
     elif LLM_PROVIDER == "gemini":
-        """Carga y devuelve el modelo LLM de Gemini."""
-        if 'GOOGLE_API_KEY' not in os.environ:
-            raise ValueError("La variable de entorno GOOGLE_API_KEY no está configurada.")
-        print(f"Inicializando LLM de Gemini: {MODEL_NAME_LLM}...")
-        try:
-            llm = ChatGoogleGenerativeAI(model=MODEL_NAME_LLM, 
-                                         temperature=TEMPERATURE,
-                                         google_api_key=os.getenv("GOOGLE_API_KEY"))
-            return llm
-        except Exception as e:
-            print(f"Error al inicializar LLM de Gemini: {e}")
-            raise
+        api_key = st.secrets["GOOGLE_API_KEY"]
+        return ChatGoogleGenerativeAI(
+            model=MODEL_NAME_LLM,
+            temperature=TEMPERATURE,
+            google_api_key=api_key
+            )
     else:
         raise ValueError(f"Proveedor de LLM no soportado: {LLM_PROVIDER}")
 
